d39f7b3cf3a065daf76844677064468031a1685f
isoliveira20 <izabeladesouzaoliveira@gmail.com>
2025-05-14 09:11:25 -0300

    Update challenge.py

--- challenge.py
+++ challenge.py
@@ -32,24 +32,23 @@ print(df.duplicated().sum()) #imprime a soma de valores duplicados por coluna
 #sns.pairplot(df)
 #plt.show()
 
-
 # Tratamento de variáveis categóricas p/ numéricas usando LabelEncoder
 label_encoder = LabelEncoder()
-for col in ['Gender', 'Marital Status', 'Education Level', 'Occupation', 'Location', 'Policy Type', 'Policy Start Date', 'Customer Feedback', 'Smoking Status', 'Exercise Frequency', 'Property Type']:
+for col in ['gender', 'marital_status', 'education_level', 'occupation', 'location', 'policy_type', 'policy_start_date', 'smoking_status', 'exercise_frequency', 'property_type']:
     df[col] = label_encoder.fit_transform(df[col])
 
 print(df.head()) # Verificando a transformação
 
 # Dividir os dados em variáveis features (x) e target (y)
-x = df.drop('Premium Amount', axis=1)  # Dados de entrada - todas as colunas menos 'Premium Amount'
-y = df['Premium Amount']  # A coluna 'Premium Amount' é o target, variável que queremos prever
+x = df.drop('premium_amount', axis=1)  # Dados de entrada - todas as colunas menos 'Premium Amount'
+y = df['premium_amount']  # A coluna 'Premium Amount' é o target, variável que queremos prever
 
 # Dividindo o conjunto de dados em treino e teste (80% treino, 20% teste)
 X_train, X_test, y_train, y_test= train_test_split(x, y, test_size=0.2, random_state=42)
 
 # Dados de treino - Normalizar os dados numéricos (colocar tudo na mesma escala — geralmente de 0 a 1)
 scaler = MinMaxScaler() # Inicializa o MinMaxScaler
-features = df.drop('Premium Amount', axis=1).columns # Nome das colunas dos dados de entrada
+features = df.drop('premium_amount', axis=1).columns # Nome das colunas dos dados de entrada
 X_train_scaled = scaler.fit_transform(X_train)  # aprende (fit) e aplica (transform) no treino
 X_test_scaled = scaler.transform(X_test)        # aprende (fit) e aplica (transform) a mesma escala no teste
 df_scaled = scaler.fit_transform(df[features]) # Cria um novo DataFrame com os dados normalizados
